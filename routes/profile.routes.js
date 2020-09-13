e71c458a4d2c0a862bb2af2897e7a75fd062eccf
Sylwia <sylwia_24_24@wp.pl>
2020-09-13 09:22:07 +0200

    Update v

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
+  //req.logIn();
   res.render('profile');
 });
 
@@ -14,5 +15,15 @@ router.get('/settings', (req, res) => {
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 }); 
+
+router.get('/user/profile/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/profile');
+
+  }
+);
 
 module.exports = router;
